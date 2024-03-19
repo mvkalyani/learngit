875fba596d694d59055b31a7649df26146111fac
uvsuresh <uvssuresh@gmail.com>
2024-03-19 16:19:25 +0530

    age and name commite

---
+++ javaproject/src/javaproject/Age.java
@@ -0,0 +1,11 @@
+package javaproject;
+
+public class Age {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 28;
+		System.out.println("Age:"+age);
+	}
+
+}
---
+++ javaproject/src/javaproject/Name.java
@@ -0,0 +1,11 @@
+package javaproject;
+
+public class Name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "kalyani";
+		System.out.println("Name:"+name);
+	}
+
+}
