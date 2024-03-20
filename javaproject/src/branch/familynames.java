1b3339400390410036752b347cefe6f41c4c3c62
mvkalyani <mveerakalyani@gmail.com>
2024-03-20 11:38:33 +0530

    2 commit family name

---
+++ javaproject/src/branch/familynames.java
@@ -0,0 +1,15 @@
+package branch;
+
+public class familynames {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String momName ="Nature";
+		String dadName ="water";
+		String childName ="Tress";
+		System.out.println("momName :"+momName);
+		System.out.println("dadName :"+dadName);
+		System.out.println("ChildName :"+childName);
+	}
+
+}
