58435d0dc4ca700433aa782db9880b65ea9963bc
mvkalyani <mveerakalyani@gmail.com>
2024-03-20 13:07:52 +0530

    3rd file animal coomit

---
+++ javaproject/src/pet/Animal.java
@@ -0,0 +1,13 @@
+package pet;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String animal ="pet dog";
+		System.out.println("animal:"+animal);
+	}
+
+}
+
+
